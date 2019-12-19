1d7c40b8776c029a3b5b1d4381aa86459964c3a0
susiexia <57109686+susiexia@users.noreply.github.com>
2019-12-19 00:02:37 -0800

    extract datasets for scatter plots

--- PyBer.py
+++ PyBer.py
@@ -43,4 +43,30 @@ pyber_data_df.head(10)
 # make sure wether the right dataframe info into marge
 city_data_df.loc[(city_data_df['city']=='Lake Jonathanshire')]
 
-# %%
+# %% [markdown]
+## BUBBLE PLOTS
+# %% [markdown]
+### Use filter to create 3 new DF for 3 citty types
+urban_cities_df = pyber_data_df.loc[(pyber_data_df['type']=='Urban')]
+rural_cities_df = pyber_data_df.loc[(pyber_data_df['type']=='Rural'),:]
+suburban_cities_df = pyber_data_df[pyber_data_df["type"] == "Suburban"]
+
+# %%[markdown]
+### get x_axis array (the numbers of rides per city per type)
+urban_ride_numbers_Series = urban_cities_df.groupby(urban_cities_df['city']).ride_id.count()
+rural_ride_numbers_Series = rural_cities_df.groupby(rural_cities_df['city']).ride_id.count()
+suburban_ride_numbers_Series = suburban_cities_df.groupby(suburban_cities_df['city']).ride_id.count()
+
+# %% [markdown]
+### get y_axis array (the average fare per city per type)
+urban_avg_fare_Series = urban_cities_df.groupby(urban_cities_df['city']).fare.agg('mean')
+rural_avg_fare_Series = rural_cities_df.groupby(["city"]).mean()["fare"]
+suburban_avg_fare_Series =suburban_cities_df.groupby(["city"]).mean()["fare"]
+
+# %% [markdown]
+### get markersize array (the average driver per city per type)
+urban_avg_driver_Series = urban_cities_df.groupby(urban_cities_df['city']).mean()['driver_count']
+rural_avg_driver_Series = rural_cities_df.groupby(rural_cities_df['city']).mean()['driver_count']
+suburban_avg_driver_Series = suburban_cities_df.groupby(suburban_cities_df['city']).mean()['driver_count']
+
+# %% [markdown]
